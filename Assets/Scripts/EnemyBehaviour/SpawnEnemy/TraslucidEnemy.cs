9714ad50ad4474f983f55763e661126b990c8cb4
3 <samuelgtz025@gmail.com>
2026-03-21 23:53:20 -0600

    sonido al desaparecer enemy
    ya jala

--- Assets/Scripts/EnemyBehaviour/SpawnEnemy/TraslucidEnemy.cs
+++ Assets/Scripts/EnemyBehaviour/SpawnEnemy/TraslucidEnemy.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class TraslucidEnemy : MonoBehaviour
 {
     [Header("Audio")]
-    public AudioClip disappearSound; //clip se sonido de enemigo desaparecer
+    private AudioSource audioSource;
 
-    private void OnCollisionEnter(Collision collision)
+    public AudioClip disappearSound;
+    
+
+    private void Start()
+    {
+        //audiosource = GetComponent<AudioSource>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         // Si el objeto que chocó tiene la etiqueta "Player"
-        if (collision.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-          
-            SoundControllerEnemy.Instance.playSound(disappearSound);
+            
+            //SoundControllerEnemy.Instance.playSound(disappearSound);
+            AudioSource.PlayClipAtPoint(disappearSound, Camera.main.transform.position);
+
             gameObject.SetActive(false);
         }
     }
+    
 }
