0dd3ac94db3a5ff2405c1c4aa753a820a42c6536
3 <samuelgtz025@gmail.com>
2026-04-10 10:03:49 -0600

    MUERTE A APT

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -30,7 +30,7 @@ public class PlayerHealth : MonoBehaviour
 
         if (lifePoints >= 180)
         {
-            SceneManager.LoadScene("Stairs");
+            SceneManager.LoadScene("LvlSelectorAPT");
             // Reemplaza "NombreDeTuEscena" con el nombre exacto de la escena que quieres cargar
         }
     }
