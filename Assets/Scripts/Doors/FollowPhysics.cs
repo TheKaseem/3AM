649af4eb55e01533a83e92d93168de576ee366f4
Kaseem <chavarria.martinez.victor101@gmail.com>
2026-04-29 16:36:01 -0600

    Door System V1
    Super buggy

---
+++ Assets/Scripts/Doors/FollowPhysics.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FollowPhysics : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.MovePosition(target.transform.position);
+    }
+}
