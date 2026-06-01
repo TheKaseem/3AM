e63566440e814cb304dddf83d7e1ca8fc0b2776c
Zniver4 <jonathan4zacos@gmail.com>
2026-05-31 23:50:58 -0600

    escena de muerte arreglada

--- Assets/Scripts/Deathscene/SimpleAnimation.cs
+++ Assets/Scripts/Deathscene/SimpleAnimation.cs
@@ -11,26 +11,24 @@ public class SimpleAnimation : MonoBehaviour
     public AudioSource audioSource;
     public AudioClip clickSound;
 
-    [Header("Animación hacia el jugador")]
-    public Transform objeto;          
-    public Transform destinoJugador; 
-    public float duracion = 2f;       
+    [Header("Animación hacia el punto destino")]
+    public Transform objeto;
+    public Transform puntoDestino; // Nuevo destino específico
+    public float duracion = 2f;
 
     public void StartAudio()
     {
-        Debug.Log("Botón StartTutorial fue presionado");
+        Debug.Log("Botón fue presionado");
         StartCoroutine(PlaySoundAnimate());
     }
 
     private IEnumerator PlaySoundAnimate()
     {
-
         if (audioSource != null && clickSound != null)
         {
             audioSource.PlayOneShot(clickSound);
             yield return new WaitForSeconds(clickSound.length);
         }
-
 
         Vector3 inicio = objeto.position;
         float tiempoInicio = Time.time;
@@ -38,14 +36,12 @@ public class SimpleAnimation : MonoBehaviour
         while (Time.time - tiempoInicio < duracion)
         {
             float t = (Time.time - tiempoInicio) / duracion;
-            objeto.position = Vector3.Lerp(inicio, destinoJugador.position, t);
+            objeto.position = Vector3.Lerp(inicio, puntoDestino.position, t);
             yield return null;
         }
 
-
-        objeto.position = destinoJugador.position;
+        objeto.position = puntoDestino.position;
         Debug.Log("Animación completada, cargando escena: " + sceneName);
-
 
         SceneManager.LoadScene(sceneName);
     }
